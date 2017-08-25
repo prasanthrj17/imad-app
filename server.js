54eb621d052314d074207c617ecc0312d86a7804
prasanth raj r p <30790493+prasanthrj17@users.noreply.github.com>
2017-08-25 14:08:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+app.get("/",function (req,res)){
+  res.sendfile(path.join(__dirname, "ui", 'index.html'));  
+});
 
 app.get('/', function (req, res) {
     res.send('Article one requested and will be served here');
